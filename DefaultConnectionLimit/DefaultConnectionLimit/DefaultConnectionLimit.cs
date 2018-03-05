03ea4819cc6b8055707482562f78e8a6dc4e6bf3
Stephan Adler <stephanadler1@users.noreply.github.com>
2018-03-05 07:30:32 -0800

    Demonstration of a bug in .NET Framework 4.7.1. ServicePoint specific limits are not being applied to the connection. Instead they are being overwritten by the default limmits.

---
+++ DefaultConnectionLimit/DefaultConnectionLimit/DefaultConnectionLimit.cs
@@ -0,0 +1,94 @@
+﻿namespace DefaultConnectionLimit
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Microsoft.Win32;
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// This demonstrates a bug that got introduced with the .NET Framework 4.7.1 update where
+    /// <see cref="ServicePoint.ConnectionLimit"/>s are no longer applied to new connections being
+    /// made with the <see cref="HttpClient"/>, but the <see cref="ServicePointManager.DefaultConnectionLimit"/>
+    /// is being applied instead.
+    /// </summary>
+    [TestClass]
+    public class DefaultConnectionLimit
+    {
+        public TestContext TestContext { get; set; }
+
+        /// <summary>
+        /// This test behaves differently depending on the version of .NET Framework installed on the machine.
+        /// If running with .NET prior 4.7.1 the test succeeds, if running with 4.7.1 it fails.
+        /// This is irrespective of the way the connection limits are being configured. It can also be 
+        /// reproduced if configured in an app.config with 
+        ///     <connectionManagement>
+        ///       <clear/>
+        ///       <add address="https://www.bing.com" maxconnection="20"/>
+        ///       <add address="*" maxconnection="10"/>
+        ///     </connectionManagement>
+        /// </summary>
+        [TestMethod]
+        public async Task ConnectionSpecificConnectionLimitsAreHonored()
+        {
+            DetermineDotNetFramework();
+
+            // Set the default value to something other than 2 or 4.
+            const int defaultConnectionLimit = 10;
+            const int bingConnectionLimit = 20;
+
+            Assert.AreNotEqual(defaultConnectionLimit, bingConnectionLimit, "Please choose different limits.");
+
+            var uri = new Uri("https://www.bing.com");
+            ServicePointManager.DefaultConnectionLimit = defaultConnectionLimit;
+            ServicePoint sp = ServicePointManager.FindServicePoint(uri);
+            sp.ConnectionLimit = bingConnectionLimit;
+
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = uri;
+
+                HttpResponseMessage response = await httpClient.GetAsync("/").ConfigureAwait(false);
+
+                sp = ServicePointManager.FindServicePoint(uri);
+                Assert.AreEqual(bingConnectionLimit, sp.ConnectionLimit, $"If run with .NET 4.7.1 installed on the machine, this check fails and instead sp.ConnectionLimit == ServicePointManager.DefaultConnectionLimit ({defaultConnectionLimit})");
+            }
+        }
+
+        private void DetermineDotNetFramework()
+        {
+            using (RegistryKey localMachineHive = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Default))
+            using (RegistryKey frameworKey = localMachineHive.OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full"))
+            {
+                object releaseObj = frameworKey?.GetValue("Release", null);
+                if (releaseObj != null)
+                {
+                    // https://docs.microsoft.com/en-us/dotnet/framework/migration-guide/how-to-determine-which-versions-are-installed
+                    switch ((int)releaseObj)
+                    {
+                        case 460798:
+                            this.TestContext.WriteLine(".NET Framework 4.7 with Creators Update on Windows 10");
+                            break;
+
+                        case 460805:
+                            this.TestContext.WriteLine(".NET Framework 4.7");
+                            break;
+
+                        case 461308:
+                            this.TestContext.WriteLine(".NET Framework 4.7.1 with Fall Creators Update on Windows 10");
+                            break;
+
+                        case 461310:
+                            this.TestContext.WriteLine(".NET Framework 4.7.1");
+                            break;
+
+                        default:
+                            this.TestContext.WriteLine($"Undetermined: {(int)releaseObj}");
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ DefaultConnectionLimit/DefaultConnectionLimit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DefaultConnectionLimit")]
+[assembly: AssemblyDescription("Demonstrate the default connection limit bug in .NET Framework 4.7.1")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Stephan Adler")]
+[assembly: AssemblyProduct("DefaultConnectionLimit")]
+[assembly: AssemblyCopyright("Copyright © 2017 Stephan Adler")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f5c7f099-3ccc-4d4d-9ab7-bcc7ffa96ec2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
